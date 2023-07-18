269c8b8cde4b4708345687a50a49ca74c391fd63
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2023-07-18 21:25:50 +0800

    up

--- lib/ui/views/card_slots/card_slots_view.dart
+++ lib/ui/views/card_slots/card_slots_view.dart
@@ -43,174 +43,186 @@ class CardSlotsView extends StackedView<CardSlotsViewModel> {
           ],
         ),
         body: LayoutBuilder(builder: (context, size) {
-          return GestureDetector(
-            onVerticalDragDown: (details) {
-              viewModel.selectedSlotId = null;
-            },
-            onTap: () {},
-            child: SingleChildScrollView(
-              padding: Dimens.computedWidth(
-                  screenSize: size, targetWidth: 500, hPadding: 0, vPadding: 0),
-              child: Column(
-                children: [
-                  vSpaceSmall,
-                  Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey, width: 0.5)),
-                    child: Center(
-                        child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  "${card.teamOneScore} ".toUpperCase(),
-                                  textAlign: TextAlign.end,
-                                  style: const TextStyle(
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                              Text(
-                                "   -   ".toUpperCase(),
+          return SingleChildScrollView(
+            padding: Dimens.computedWidth(
+                screenSize: size, targetWidth: 500, hPadding: 0, vPadding: 0),
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 0.5)),
+                  child: Center(
+                      child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Text(
+                                "${card.teamOneScore} ".toUpperCase(),
+                                textAlign: TextAlign.end,
                                 style: const TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.bold),
+                                    color: Colors.red,
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold),
                               ),
-                              Expanded(
-                                child: Text(
-                                  "${card.teamTwoScore}".toUpperCase(),
-                                  style: const TextStyle(
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.bold),
-                                ),
+                            ),
+                            Text(
+                              "   -   ".toUpperCase(),
+                              style: const TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            Expanded(
+                              child: Text(
+                                "${card.teamTwoScore}".toUpperCase(),
+                                style: const TextStyle(
+                                    color: Colors.red,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold),
                               ),
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  "${card.teamOneName} ".toUpperCase(),
-                                  textAlign: TextAlign.end,
-                                  style: const TextStyle(
-                                      height: 1.1,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                              Text(
-                                "  vs  ".toUpperCase(),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: Text(
+                                "${card.teamOneName} ".toUpperCase(),
+                                textAlign: TextAlign.end,
                                 style: const TextStyle(
                                     height: 1.1,
                                     fontSize: 20,
                                     fontWeight: FontWeight.bold),
                               ),
-                              Expanded(
-                                child: Text(
-                                  "${card.teamTwoName}".toUpperCase(),
-                                  style: const TextStyle(
-                                      height: 1.1,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              "  vs  ".toUpperCase(),
+                              style: const TextStyle(
+                                  height: 1.1,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Expanded(
+                              child: Text(
+                                "${card.teamTwoName}".toUpperCase(),
+                                style: const TextStyle(
+                                    height: 1.1,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ],
+                        ),
+                        if (card.title != null)
+                          Text((card.title ?? "").toTitleCase()),
+                        vSpaceSmall,
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Wrap(
+                                  crossAxisAlignment: WrapCrossAlignment.center,
+                                  spacing: 4,
+                                  runSpacing: 4,
+                                  children: [
+                                    Icon(Icons.circle_rounded,
+                                        color: Colors.blue),
+                                    Text("Paid: 1")
+                                  ],
                                 ),
-                              ),
-                            ],
-                          ),
-                          if (card.title != null)
-                            Text((card.title ?? "").toTitleCase()),
-                          vSpaceSmall,
-                          const Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Wrap(
-                                crossAxisAlignment: WrapCrossAlignment.center,
-                                spacing: 4,
-                                runSpacing: 4,
-                                children: [
-                                  Icon(Icons.circle_rounded,
-                                      color: Colors.blue),
-                                  Text("Paid: 1")
-                                ],
-                              ),
-                              Wrap(
-                                crossAxisAlignment: WrapCrossAlignment.center,
-                                spacing: 4,
-                                runSpacing: 4,
-                                children: [
-                                  Icon(Icons.circle_rounded,
-                                      color: Colors.grey),
-                                  Text("Unpaid: 2")
-                                ],
-                              ),
-                              Wrap(
-                                crossAxisAlignment: WrapCrossAlignment.center,
-                                spacing: 4,
-                                runSpacing: 4,
-                                children: [
-                                  Icon(Icons.circle_rounded, color: Colors.red),
-                                  Text("Slots left: ")
-                                ],
-                              ),
-                              Wrap(
-                                crossAxisAlignment: WrapCrossAlignment.center,
-                                spacing: 4,
-                                runSpacing: 4,
-                                children: [
-                                  Icon(Icons.circle_rounded,
-                                      color: Colors.orangeAccent),
-                                  Text("Winner: ")
-                                ],
-                              ),
-                              Wrap(
-                                crossAxisAlignment: WrapCrossAlignment.center,
-                                spacing: 4,
-                                runSpacing: 4,
-                                children: [
-                                  Icon(Icons.money_rounded,
-                                      color: Colors.green),
-                                  Text("Bet: 100 ")
-                                ],
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                    )),
-                  ),
-                  vSpaceSmall,
-                  Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey, width: 0.5)),
-                    child: GridView.builder(
-                      shrinkWrap: true,
-                      physics: const ScrollPhysics(),
-                      padding: EdgeInsets.zero,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCountAndFixedHeight(
-                              crossAxisCount: 4,
-                              crossAxisSpacing: 0,
-                              mainAxisSpacing: 0,
-                              height: 64),
-                      itemCount: viewModel.slots.length,
-                      itemBuilder: (context, index) {
-                        final item = viewModel.slots[index];
-                        return SlotItem(item,
-                            isSelected: viewModel.selectedSlotId == item.id,
-                            onTap: (slot) {
-                          viewModel.updateSlot(
-                              slot,
-                              (item.name?.isNotEmpty ?? false)
-                                  ? ActionType.update
-                                  : ActionType.add);
-                        });
-                      },
+                                Wrap(
+                                  crossAxisAlignment: WrapCrossAlignment.center,
+                                  spacing: 4,
+                                  runSpacing: 4,
+                                  children: [
+                                    Icon(Icons.circle_rounded,
+                                        color: Colors.grey),
+                                    Text("Unpaid: 2")
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Wrap(
+                                  crossAxisAlignment: WrapCrossAlignment.center,
+                                  spacing: 4,
+                                  runSpacing: 4,
+                                  children: [
+                                    Icon(Icons.circle_rounded,
+                                        color: Colors.red),
+                                    Text("Slots left: ")
+                                  ],
+                                ),
+                                Wrap(
+                                  crossAxisAlignment: WrapCrossAlignment.center,
+                                  spacing: 4,
+                                  runSpacing: 4,
+                                  children: [
+                                    Icon(Icons.circle_rounded,
+                                        color: Colors.orangeAccent),
+                                    Text("Winner: ")
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Wrap(
+                                  crossAxisAlignment: WrapCrossAlignment.center,
+                                  spacing: 4,
+                                  runSpacing: 4,
+                                  children: [Text("Bet: 100 ")],
+                                ),
+                                Wrap(
+                                  crossAxisAlignment: WrapCrossAlignment.center,
+                                  spacing: 4,
+                                  runSpacing: 4,
+                                  children: [Text("Prize: 100 ")],
+                                ),
+                              ],
+                            )
+                          ],
+                        )
+                      ],
                     ),
+                  )),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 0.5)),
+                  child: GridView.builder(
+                    shrinkWrap: true,
+                    physics: const ScrollPhysics(),
+                    padding: EdgeInsets.zero,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCountAndFixedHeight(
+                            crossAxisCount: 4,
+                            crossAxisSpacing: 0,
+                            mainAxisSpacing: 0,
+                            height: 64),
+                    itemCount: viewModel.slots.length,
+                    itemBuilder: (context, index) {
+                      final item = viewModel.slots[index];
+                      return SlotItem(item,
+                          isSelected: viewModel.selectedSlotId == item.id,
+                          onTap: (slot) {
+                        viewModel.updateSlot(
+                            slot,
+                            (item.name?.isNotEmpty ?? false)
+                                ? ActionType.update
+                                : ActionType.add);
+                      });
+                    },
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           );
         }),
